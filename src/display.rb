8ff732888916dce5125726b1d2939abadf057b67
codebambu <codebambu@protonmail.ch>
2020-07-03 21:44:21 +0200

    remove comments

--- src/display.rb
+++ src/display.rb
@@ -32,7 +32,4 @@ class Display
 
     print output
   end
-  # methods like paint/draw to draw input (from map)
-  # methods like clear to clear the screen
-  # everything related to displaying the map basically
 end
